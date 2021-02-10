32a5e1a3530c1c5b2c27aa8096193fb5dcd601f2
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-02-10 18:15:16 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		stack< char, vector<char> > iStack;
+		string x;
+		cin >> x;
+		int cnt = 0;
+		for (int i = 0; i < (int) x.size(); i++) {
+			if (x[i] == '(' || x[i] == '{' || x[i] == '[') {
+				cnt++;
+				iStack.push(x[i]);
+			} else {
+				if (iStack.empty()) {
+					break;
+				}
+				if (x[i] == ')') {
+					if (iStack.top() == '(') {
+						cnt++;
+						if (!iStack.empty()) {
+							iStack.pop();
+						}
+					}
+				} else if (x[i] == '}') {
+					if (iStack.top() == '{') {
+						cnt++;
+						if (!iStack.empty()) {
+							iStack.pop();
+						}
+					}
+				} else if (x[i] == ']') {
+					if (iStack.top() == '[') {
+						cnt++;
+						if (!iStack.empty()) {
+							iStack.pop();
+						}
+					}
+				}
+			}
+		}
+		cout << (iStack.empty() && cnt == (int) x.size()? "YES" : "NO") << '\n';
+	}
+	return 0;
+}
